123498b8a9c46ff8a4351c0022199a12a7cf55e5
mallamsripooja <mallamsripooja@gmail.com>
2018-03-16 13:50:00 +0530

    Added dealer servlet

--- src/main/java/com/onlinecatalogapp/elements/ItemStatus.java
+++
@@ -1,7 +0,0 @@
-package com.onlinecatalogapp.elements;
-
-public enum ItemStatus {
-	AVAILABLE,
-	OUT_OF_STOCK,
-	LESS_STOCK
-}
--- src/main/java/com/onlinecatalogapp/sql/SQLStatements.java
+++
@@ -1,7 +0,0 @@
-package com.onlinecatalogapp.sql;
-
-public class SQLStatements {
-	private SQLStatements() {}
-	
-	public static final String SQL_CATEGORY_INSERT_STMT = "INSERT INTO CATEGORY (catname) VALUES (?)";
-}
